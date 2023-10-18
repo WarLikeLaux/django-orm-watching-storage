7713f587b78007ff56efd47ff1b7aa717b5ea3e7
WarLikeLaux <warlikelaux080399@mail.ru>
2023-10-19 02:11:29 +0600

    Fixed passcard info viewing

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,21 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Passcard, Visit, format_duration
 from django.shortcuts import render
+from django.utils.timezone import localtime
+from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    passcard_visits_details = []
+    this_passcard_visits = Visit.objects.filter(passcard=passcard).all()
+    for visit in this_passcard_visits:
+        passcard_visits_details.append({
+            'entered_at': localtime(visit.entered_at).strftime('%d-%m-%Y'),
+            'duration': format_duration(visit.get_duration()),
+            'is_strange': visit.is_long()
+        })
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': passcard_visits_details
     }
     return render(request, 'passcard_info.html', context)
